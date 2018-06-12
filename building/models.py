7865677acccd9f50f7bf189f1cef3ac513195ac9
trowa88 <trowa020@gmail.com>
2018-06-12 17:44:05 +0900

    slug 제거, Building 모델 creator 추가

--- building/models.py
+++ building/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from django.utils.text import slugify
 
 from core.models import TimeStampedEnabledModel, Cities
 
 
 class Building(TimeStampedEnabledModel):
     city = models.ForeignKey(Cities, on_delete=models.DO_NOTHING, null=False)
-    slug = models.SlugField(unique=True, blank=True)
     name = models.CharField(max_length=50)
     desc = models.CharField(max_length=100, blank=True)
     address = models.CharField(max_length=50)
     img_src = models.ImageField(upload_to='buildings/', default=None)
+    creator = models.ForeignKey('users.User', on_delete=models.DO_NOTHING, null=False,
+                                default=None)
 
     class Meta:
         db_table = 'building'
@@ -18,7 +18,3 @@ class Building(TimeStampedEnabledModel):
 
     def __str__(self):
         return self.name
-
-    def save(self, force_insert=False, force_update=False, using=None,
-             update_fields=None):
-        self.slug = slugify(self.name)
