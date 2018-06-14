8959464fb60ab42a6f6db05ec67c6dacb5778056
trowa88 <trowa020@gmail.com>
2018-06-14 19:04:08 +0900

    Add BuildingHistory

--- building/models.py
+++ building/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from core.models import TimeStampedEnabledModel, Cities
 
 
-class Building(TimeStampedEnabledModel):
+class AbstractBuilding(TimeStampedEnabledModel):
     city = models.ForeignKey(Cities, on_delete=models.DO_NOTHING, null=False)
     name = models.CharField(max_length=50)
     desc = models.CharField(max_length=100, blank=True)
@@ -13,8 +13,21 @@ class Building(TimeStampedEnabledModel):
                                 default=None)
 
     class Meta:
+        abstract = True
+
+
+class Building(AbstractBuilding):
+    class Meta:
         db_table = 'building'
         ordering = ['-updated']
 
     def __str__(self):
         return self.name
+
+
+class BuildingHistory(AbstractBuilding):
+    building = models.ForeignKey(Building, on_delete=models.DO_NOTHING, null=False)
+
+    class Meta:
+        db_table = 'building_history'
+        ordering = ['-pk']
