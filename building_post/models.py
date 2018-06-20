f2b1426d025de8b9161b949a38f41a71fbaac83a
trowa88 <trowa020@gmail.com>
2018-06-20 14:38:57 +0900

    Add BuildingPostHistory Model, migrations

--- building_post/models.py
+++ building_post/models.py
@@ -4,15 +4,28 @@ from building.models import Building
 from core.models import TimeStampedEnabledModel
 
 
-class BuildingPost(TimeStampedEnabledModel):
+class AbstractBuildingPost(TimeStampedEnabledModel):
     building = models.ForeignKey(Building, on_delete=models.DO_NOTHING, null=False)
     creator = models.ForeignKey('users.User', on_delete=models.DO_NOTHING, null=False)
     title = models.CharField(max_length=100)
     content = models.TextField()
 
     class Meta:
+        abstract = True
+
+
+class BuildingPost(AbstractBuildingPost):
+    class Meta:
         db_table = 'building_post'
         ordering = ['-updated']
 
     def __str__(self):
         return self.title
+
+
+class BuildingPostHistory(AbstractBuildingPost):
+    building_post = models.ForeignKey('BuildingPost', on_delete=models.DO_NOTHING, null=False)
+
+    class Meta:
+        db_table = 'building_post_history'
+        ordering = ['-pk']
